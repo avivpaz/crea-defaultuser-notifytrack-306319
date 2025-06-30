73fa96ad00912c1f79369fd44622903a9486f2a0
Aviv Paz <avivpaz43556@gmail.com>
2025-07-01 00:03:24 +0300

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5D3FD3',
+        secondary: '#F8F9FA',
+      },
+    },
+  },
+  plugins: [],
+} 
